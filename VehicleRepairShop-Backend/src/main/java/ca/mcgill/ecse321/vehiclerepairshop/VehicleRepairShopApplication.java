91272b9d8db649ab1ad8131e55e93a917de20474
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2021-02-17 17:44:56 +0800

    Initial commit of the backend application

---
+++ VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/VehicleRepairShopApplication.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.vehiclerepairshop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@SpringBootApplication
+public class VehicleRepairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRepairShopApplication.class, args);
+	}
+
+	@RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+
+}
