8b18d97da5be39636f95a0e538ddca21b3173805
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2021-02-22 02:19:01 +0800

    Update CarRepository.java

--- VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/dao/CarRepository.java
+++ VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/dao/CarRepository.java
@@ -1,9 +1,14 @@
 package ca.mcgill.ecse321.vehiclerepairshop.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.vehiclerepairshop.model.Appointment;
 import ca.mcgill.ecse321.vehiclerepairshop.model.Car;
+import ca.mcgill.ecse321.vehiclerepairshop.model.Customer;
 
 public interface CarRepository extends CrudRepository<Car, String>{
-
+	List<Car> findCarByCustomer(Customer customer);
 	Car findCarByLicensePlate(String licence);
 }
