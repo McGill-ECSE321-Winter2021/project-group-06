0cd5a48b567b540289d2fae257d2f4c586761434
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2021-03-12 01:08:17 +0800

    TestBusinessInfoPersistence started

---
+++ VehicleRepairShop-Backend/src/test/java/ca/mcgill/ecse321/vehiclerepairshop/dao/TestBusinessInfoPersistence.java
@@ -0,0 +1,24 @@
+package ca.mcgill.ecse321.vehiclerepairshop.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.vehiclerepairshop.model.BusinessInformation;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestBusinessInfoPersistence {
+	
+}
