f686570ccf0f663305381f3bf266ed69daacb9c2
Catherine <catherine.caron2@mail.mcgill.ca>
2021-03-11 19:32:56 -0500

    Update OfferedServiceController.java

--- VehicleRepairShop-Backend/src/main/java/vehiclerepairshop/controller/OfferedServiceController.java
+++ VehicleRepairShop-Backend/src/main/java/vehiclerepairshop/controller/OfferedServiceController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import vehiclerepairshop.service.VehicleRepairShopService;
 
 @CrossOrigin(origins = "*")
 @RestController
