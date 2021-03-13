e1c11fac975edf23e90344cffdaceb9e4bd9658e
james <jedarby@shaw.ca>
2021-03-12 17:44:21 -0800

    car methods to public from package private

--- VehicleRepairShop-Backend/src/test/java/vehiclerepairshop/service/TestCarService.java
+++ VehicleRepairShop-Backend/src/test/java/vehiclerepairshop/service/TestCarService.java
@@ -1,5 +1,9 @@
 package vehiclerepairshop.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class TestCarService {
 
 }
