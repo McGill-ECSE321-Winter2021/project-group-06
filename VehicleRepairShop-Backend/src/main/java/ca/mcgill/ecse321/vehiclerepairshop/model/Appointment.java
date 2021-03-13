5a38bd2589455ce16c8e7c3efaa5b533a7493306
Aurelia <aurelia.haas@mail.mcgill.ca>
2021-03-13 16:23:42 -0500

    Modified some variable types and removed everything linked to date and time in the Appointment
    int to Integer
    And some clean code

--- VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/model/Appointment.java
+++ VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/model/Appointment.java
@@ -2,8 +2,6 @@ package ca.mcgill.ecse321.vehiclerepairshop.model;
 
 
 import java.util.*;
-import java.sql.Time;
-import java.sql.Date;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -15,13 +13,8 @@ import javax.persistence.Entity;
 @Entity
 public class Appointment
 {
-
-
-  private Time startTime;
-  private Time endTime;
-  private Date startDate;
-  private Date endDate;
-  private int appointmentId;
+	
+  private Integer appointmentId;
   private String comment;
   private Car car;
   private Garage garage;
@@ -37,48 +30,16 @@ public class Appointment
 	  this.comment = aComment;
 
   }
-  public void setEndDate(Date endDate){
-	  this.endDate = endDate;
-  }
-  public void setEndTime(Time endTime) {
-	  this.endTime = endTime;
-  }
 
   public void setAppointmentId(int aAppointmentId)
   {
 	  this.appointmentId = aAppointmentId;
   }
 
-  public void setStartTime(Time startTime) {
-	  this.startTime = startTime;
-  }
-  
-  public void setStartDate(Date startDate) {
-	  this.startDate = startDate;
-  }
-  public Time getStartTime(){
-		return this.startTime;
-	}
-
   @Id
   public int getAppointmentId()
   {
     return appointmentId;
-  }
-
-  public Time getEndTime()
-  {
-    return endTime;
-  }
-
-  public Date getStartDate()
-  {
-    return startDate;
-  }
-
-  public Date getEndDate()
-  {
-    return this.endDate;
   }
 
   public String getComment()
