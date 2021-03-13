57bbe4c28bb55060221ca284dfbee64ad2f80b95
james <jedarby@shaw.ca>
2021-03-12 17:27:26 -0800

    car DTO and service

--- VehicleRepairShop-Backend/src/main/java/vehiclerepairshop/dto/CarDto.java
+++ VehicleRepairShop-Backend/src/main/java/vehiclerepairshop/dto/CarDto.java
@@ -1,5 +1,86 @@
 package vehiclerepairshop.dto;
 
+import java.util.Collections;
+import java.util.List;
+
+import ca.mcgill.ecse321.vehiclerepairshop.model.CustomerAccount;
+
 public class CarDto {
+	public enum MotorType { Electric, Hybrid, Gas, Diesel }
+
+
+	  private String licensePlate;
+	  private String model;
+	  private int year;
+	  private MotorType motorType;
+
+	  private CustomerAccount owner;
+	  private List<AppointmentDto> appointmentsDTO;
+	  
+	  CarDto(){
+		  
+	  }
+	  
+	  @SuppressWarnings("unchecked")
+	  CarDto(String licensePlate, String model, int year, MotorType motorType, CustomerAccount owner){
+		  this(licensePlate, model, year, motorType, owner, Collections.EMPTY_LIST);  
+	  }
+	  
+	  CarDto(String licensePlate, String model, int year, MotorType motorType, CustomerAccount owner, List<AppointmentDto> appointments){
+		  this.licensePlate = licensePlate;
+		  this.model = model;
+		  this.year = year;
+		  this.motorType = motorType;
+		  this.owner = owner;
+		  this.appointmentsDTO = appointments;		  
+	  }
+	  
+	  String getLicensePlate(){
+		  return licensePlate;
+	  }
+	  
+	  String getModel() {
+		  return model;
+	  }
+	  
+	  int getYear() {
+		  return year;
+	  }
+	  
+	  MotorType getMotorType() {
+		  return motorType;
+	  }
+	  
+	  CustomerAccount getOwner() {
+		  return owner;
+	  }
+	  
+	  List<AppointmentDto> getAppointments(){
+		  return appointmentsDTO;
+	  }
+	  
+	  void setLicensePlate(String licensePlate) {
+		  this.licensePlate = licensePlate;
+	  }
+	  
+	  void setModel(String model) {
+		  this.model = model;
+	  }
+	  
+	  void setYear(int year) {
+		  this.year = year;
+	  }
+	  
+	  void setOwner(CustomerAccount owner) {
+		  this.owner = owner;
+	  }
+	  
+	  void setAppointments(List<AppointmentDto> appointments) {
+		  appointmentsDTO = appointments;
+	  }
+	  
+	  void addAppointments(List<AppointmentDto> appointments) {
+		  appointmentsDTO.addAll(appointments);
+	  }
 
 }
---
+++ VehicleRepairShop-Backend/src/main/java/vehiclerepairshop/service/CarService.java
@@ -0,0 +1,93 @@
+package vehiclerepairshop.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.vehiclerepairshop.dao.CarRepository;
+import ca.mcgill.ecse321.vehiclerepairshop.model.Car;
+import ca.mcgill.ecse321.vehiclerepairshop.model.Car.MotorType;
+import ca.mcgill.ecse321.vehiclerepairshop.model.CustomerAccount;
+
+public class CarService {
+	@Autowired
+	private CarRepository carRepository;
+	
+	/**
+	 * @author James Darby
+	 *
+	 */
+	
+	/*
+	 * Create a Car with given parameters
+	 * @param licensePlate
+	 * @param model
+	 * @param year
+	 * @param motorType
+	 * @return
+	 */
+	@Transactional
+	public Car createCar(String licensePlate, String model, int year, MotorType motorType) {
+		Car car = new Car();
+		car.setLicensePlate(licensePlate);
+		car.setModel(model);
+		car.setYear(year);
+		car.setMotorType(motorType);
+		carRepository.save(car);
+		return car;
+	}
+	
+	/*
+	 * gets all cars belonging to one owner
+	 * @param owner
+	 * @return
+	 */
+	@Transactional
+	public List<Car> getCarsByOwner(CustomerAccount owner) {
+		List<Car> cars = carRepository.findByOwner(owner);
+		return cars;	
+	}
+	
+	/*
+	 * gets car with the specified license plate
+	 * @param licensePlate
+	 * @return
+	 */
+	@Transactional
+	public Car getCarsByLiscensePlate(String licensePlate) {
+		Car car = carRepository.findByLicensePlate(licensePlate);
+		return car;	
+	}
+	
+	/*
+	 * returns if a customer account has a car as a boolean
+	 * @param owner
+	 * @return
+	 */
+	@Transactional
+	public boolean carExistsByOwner(CustomerAccount owner) {
+		List<Car> cars = carRepository.findByOwner(owner);
+		return cars.size() > 0;	
+	}
+	
+	/*
+	 * gets all cars in the database
+	 * @return
+	 */
+	@Transactional
+	public List<Car> getAllCars() {
+		Iterable<Car> cars = carRepository.findAll();
+		return toList(cars);	
+	}
+	
+	// Helper method that converts iterable to list
+	private <T> List<T> toList(Iterable<T> iterable){
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+	}
+
+}
