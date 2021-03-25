1a26e5ade5889e9c6b2065d26cf617515e615cd4
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2021-03-25 13:48:50 +0800

    fix

--- VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/dao/TechnicianAccountRepository.java
+++ VehicleRepairShop-Backend/src/main/java/ca/mcgill/ecse321/vehiclerepairshop/dao/TechnicianAccountRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "TechnicianAccount_data", path = "TechnicianAccount_data")
 public interface TechnicianAccountRepository extends CrudRepository<TechnicianAccount, String>{
 	List<TechnicianAccount> findTechnicianAccountByName(String name); //name is not unique
-	List<TechnicianAccount> findTechnicianAccountByAppointment(Appointment appointment);
 	TechnicianAccount findByUsername(String username); 
 	TechnicianAccount findByToken(int token);
 }
