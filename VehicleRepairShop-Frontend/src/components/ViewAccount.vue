9446b713ba4bb4a3e652049f13c7e61a3766657c
Aurelia <aurelia.haas@mail.mcgill.ca>
2021-03-27 17:12:37 -0400

    Corrected the port in Frontend and added a new View Account page
    View Account page is not finished

---
+++ VehicleRepairShop-Frontend/src/components/ViewAccount.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="eventregistration">
+    <h1>View Account</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+/* #eventregistration {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  } */
+</style>
+
