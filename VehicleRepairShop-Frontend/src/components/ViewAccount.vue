4e845575269a5f6d09c54769f01314bcfb58148a
Aurelia <aurelia.haas@mail.mcgill.ca>
2021-03-27 17:53:45 -0400

    Working on the fronted

--- VehicleRepairShop-Frontend/src/components/ViewAccount.vue
+++ VehicleRepairShop-Frontend/src/components/ViewAccount.vue
@@ -1,6 +1,18 @@
 <template>
-  <div id="eventregistration">
-    <h1>View Account</h1>
+  <div id="viewaccount">
+      
+    <h1 style="color:red">View Account </h1>
+    <button v-bind:class="{'white': !clicked, 'blue': clicked}">Manage Account </button>
+    <button>Delete Account</button>
+    <button>Log off</button>
+    <h2></h2>
+    <button>Create Time Slot</button>
+    <button>See Appointments</button>
+    <table>
+        <h3>Username: </h3>
+        <h3>Name: </h3>
+    </table>
+
   </div>
 </template>
 
@@ -9,10 +21,12 @@
 </script>
 
 <style>
-/* #eventregistration {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background: #f2ece8;
-  } */
+#viewaccount {
+    margin-bottom: 0;
+    margin-top: 0;
+    /* background-color: #a9a9a9;
+    color: #c30000; */
+  }
+
 </style>
 
