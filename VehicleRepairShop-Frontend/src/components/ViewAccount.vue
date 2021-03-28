67f175f214c3250ac3a62e783e9bec162d2fd05f
Aurelia <aurelia.haas@mail.mcgill.ca>
2021-03-27 23:22:40 -0400

    Pop Up and go back buttons
    need to run --> npm install @burhanahmeed/vue-modal-2

--- VehicleRepairShop-Frontend/src/components/ViewAccount.vue
+++ VehicleRepairShop-Frontend/src/components/ViewAccount.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="viewaccount">
-      
+    
+    <!-- <span class="go-back"> -->
+    <button @click="goBack" style="position: absolute; left: 0px">Go Back</button>
+    <button @click="goToMainMenu" style="position: absolute; right: 0px">Main Menu</button> 
     <h1 style="color:#c30000;">View Account </h1>
 
 
@@ -20,22 +23,37 @@
      <button @click="goToTest"  onclick="" type="button" style="background-color:#a9a9a9; color: white;" class="btn ">
                 <font size="3"><b>See Appointments</b></font>
     </button>
+    <h3></h3>
+    <button @click="open">Delete</button>
+    <vue-modal-2 name="modal-1" @on-close="close"> Are you sure you want to delete this account?
+    </vue-modal-2>
     <table>
 
-        <h3 style="color:#c30000;" >Username: </h3>
-        <h3 style="color:#c30000;">Name: </h3>
+        <h2 style="color:#c30000;" >Username: </h2>
+        <h2 style="color:#c30000;">Name: </h2>
     </table>
-
   </div>
 </template>
 
 <script>
        export default{
            methods:{
-               goToTest(){
-                   this.$router.push("/Test");
-               }
-           }
+            goToMainMenu(){
+              this.$router.push("/app");
+            },
+            goToTest(){
+              this.$router.push("/Test");
+            },
+            goBack(){
+              this.$router.go(-1);
+            },
+            open () {
+              this.$vm2.open('modal-1')
+            },
+            close () {
+              this.$vm2.close('modal-1')
+            }
+          }
        }
 </script>
 
--- VehicleRepairShop-Frontend/src/router/index.js
+++ VehicleRepairShop-Frontend/src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Modal from "@burhanahmeed/vue-modal-2";
 import Hello from '@/components/Hello'
 import MainMenu from '@/components/MainMenu'
 import ViewAccount from '@/components/ViewAccount'
 import Test from '@/components/Test'
 
 Vue.use(Router)
+Vue.use(Modal);
 
 export default new Router({
   routes: [
