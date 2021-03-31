dc322d8491c37e8160279793184718d115e0b746
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2021-03-31 15:11:50 +0800

    Update index.js

--- VehicleRepairShop-Frontend/src/router/index.js
+++ VehicleRepairShop-Frontend/src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Modal from "@burhanahmeed/vue-modal-2";
 import Hello from '@/components/Hello'
-
-import FirstCompoent from '@/components/FirstCompoent'
-import OfferedServiceTable from '@/components/OfferedServiceTable'
 import CalendarAdminAccount from '@/components/CalendarAdminAccount.vue'
 import MainMenu from '@/components/MainMenu'
 import ViewAccount from '@/components/ViewAccount'
@@ -12,7 +9,6 @@ import EditAccount from '@/components/EditAccount'
 import Login from '@/components/Login'
 import SignUp from '@/components/SignUp'
 import Test from '@/components/Test'
-
 
 Vue.use(Router)
 Vue.use(Modal);
@@ -77,8 +73,5 @@ export default new Router({
       component: Test
 
     }
-
-
-
   ]
 })
